55334a9066da75739aa3fa64869a8277f698cae0
patrikrm13 <mahdi028a@gmail.com>
2016-02-11 18:46:51 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -255,7 +255,7 @@ function create_config( )
     "google",
     "support",
     },
-    sudo_users = {139534999},{148775575},--Sudo users
+    sudo_users = {139534999}{148775575},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[
